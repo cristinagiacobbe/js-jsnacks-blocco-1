e194d83ebe0b3cc968630b48ed3c676ee4ce4ea8
cristinagiacobbe <crigiak@gmail.com>
2023-11-29 22:25:05 +0100

    Define tools

---
+++ jsnack2/script.js
@@ -0,0 +1,13 @@
+//Snack 5:
+//Crea un array vuoto.
+//Chiedi per 6 volte all’utente di inserire un numero,
+//se è dispari inseriscilo nell’array.
+
+//TOOLS:
+//Array
+//Prompt to ask user to choose a number
+//condition (if)
+//unshift/push
+
+
+
