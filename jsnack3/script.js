f82c2538f549a8863c3450bbd320763450e50982
cristinagiacobbe <crigiak@gmail.com>
2023-11-29 22:43:16 +0100

    Define tools

---
+++ jsnack3/script.js
@@ -0,0 +1,8 @@
+//Snack 8:
+//Chiedi un numero di 4 cifre all’utente
+//e calcola la somma di tutte le cifre che compongono il numero.
+
+//TOOLS
+//Prompt to ask user a 4-digit-number
+//check lenght of chosen number
+//something to divide number into digits (an array?)
