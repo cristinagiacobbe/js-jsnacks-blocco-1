0c6a8a0a8076c183e5e884e98aa077613f96dd6d
cristinagiacobbe <crigiak@gmail.com>
2023-11-29 22:15:25 +0100

    Define tools

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+//Snack 4:
+//In un array sono contenuti i nomi degli invitati 
+//chiedi all’utente il suo nome 
+//e comunicagli 
+//se può partecipare o no alla festa.
+
+//TOOLS:
+//Array with guest names
+//Prompt to ask a guest his name
+//loop and check(if)
+//output with result
+
